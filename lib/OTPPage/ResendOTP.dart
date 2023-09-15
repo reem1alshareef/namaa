40fdab357372841c5b533767e407840e85aff87e
Shadengd <shaden.f01h@gmail.com>
2023-09-15 22:36:09 +0300

    OTP Frontend and Edits on introPageHeader

---
+++ lib/OTPPage/ResendOTP.dart
@@ -0,0 +1,73 @@
+import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+import 'package:namaagp/OTPPage/ViewModelOTPPage.dart';
+
+class ResendOTP extends StatelessWidget {
+  // final String title;
+  // final String placeHolder;
+  final resend = new ViewModelOTPPage();
+
+   ResendOTP({
+    Key? key,
+    // required this.title,
+    // required this.placeHolder,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return GestureDetector(
+      onTap: () {
+        resend.Resend_OTP();
+              //Navigator.pop(context);
+              },
+      child: Row(
+        
+      children: [
+        // SizedBox(
+        //   width: 350,
+        //   height: 25,
+        //   child: 
+          SizedBox(width: 85,),
+          Text('إعادة الإرسال',
+              textAlign: TextAlign.right,
+              style: GoogleFonts.getFont("Noto Sans Arabic",
+                  fontSize: 16,
+                  fontWeight: FontWeight.w400,
+                  height: 1.5,
+                  textStyle: const TextStyle(color: Color(0xFF6FA8FF), decoration: TextDecoration.underline)),
+                  
+                  //textDecoration: ,
+                  ),
+                  Text('لم يصلك رمز التحقق؟  ',
+              textAlign: TextAlign.right,
+              style: GoogleFonts.getFont("Noto Sans Arabic",
+                  fontSize: 16,
+                  fontWeight: FontWeight.w400,
+                  height: 1.5,
+                  textStyle: const TextStyle(color: Color(0xFFC5C5CD)))),
+        //),
+        
+      ],
+    )
+    ,)
+    ;
+  }
+}
+// SizedBox(
+        //     width: 350,
+        //     height: 35,
+        //     child: TextFormField(
+        //       textAlign: TextAlign.right,
+        //       decoration: InputDecoration(
+        //         hintText: placeHolder,
+        //         hintStyle: TextStyle(
+        //             color: Color(0xFF898B99), height: 1, fontSize: 18),
+        //         enabledBorder: UnderlineInputBorder(
+        //           borderSide: BorderSide(
+        //               width: 0.7, color: Color(0xFFB8BAC2)), //<-- SEE HERE
+        //         ),
+        //       ),
+        //     )),
+        // SizedBox(
+        //   height: 20,
+        // )
---
+++ lib/OTPPage/ViewOTPPage.dart
@@ -0,0 +1,107 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_otp_text_field/flutter_otp_text_field.dart';
+import 'package:namaagp/Components/ReturnToPreviousPage.dart';
+import 'package:namaagp/Identity%20Elements/introPagesHeader.dart';
+import 'package:namaagp/OTPPage/ResendOTP.dart';
+import 'package:namaagp/OTPPage/ViewModelOTPPage.dart';
+import 'package:stacked/stacked.dart';
+
+
+class ViewOTPPage extends StatelessWidget {
+  const ViewOTPPage({super.key});
+  
+  get otpController => null;
+
+  @override
+  Widget build(BuildContext context) {
+    return ViewModelBuilder<ViewModelOTPPage>.reactive(
+        viewModelBuilder: () => ViewModelOTPPage(),
+        builder: (context, viewmodel, _) {
+          return Scaffold(
+              backgroundColor: const Color(0x00071121),
+              body: Container(
+                padding: const EdgeInsets.only(top: 50.0, left: 10, right: 10),
+                width: 428,
+                height: 926,
+                clipBehavior: Clip.antiAlias,
+                decoration: const BoxDecoration(
+                  //maybe delete const
+                  gradient: LinearGradient(
+                    begin: Alignment(-0.00, -6.00),
+                    end: Alignment(0, 1.5),
+                    colors: [
+                      Color(0xFF342D68),
+                      Color(0xFF352D68),
+                      Color(0x00352D68)
+                    ],
+                  ),
+                ),
+                // ignore: prefer_const_constructors
+                child: Column(
+                  children: [
+                    ReturnToPreviousPage(title: 'الصفحة السابقة'),
+                    //ReturnToPreviousPage(),
+                    //mainHeader(),
+                    introPagesHeader(subTitle: 'تم إرسال رمز التحقق على رقم جوالك', title: 'أدخل رمز التحقق',),
+            //         OTPTextField(
+            //           //wi,
+            //           otpFieldStyle: OtpFieldStyle(borderColor: Color(0xE5D0CDEF), ),
+            //           //borderColor: Color(0xFF512DA8),
+            // controller: otpController,
+            // length: 4,
+            // width: MediaQuery.of(context).size.width,
+            // textFieldAlignment: MainAxisAlignment.spaceAround,
+            // fieldWidth: 45,
+            // fieldStyle: FieldStyle.box,
+            // outlineBorderRadius: 0,
+            // style: TextStyle(fontSize: 17),
+            // onChanged: (pin) {
+            //   print("Changed: " + pin);
+            // },
+            // onCompleted: (pin) {
+            //   print("Completed: " + pin);
+            // }),
+                    OtpTextField(
+                      margin: EdgeInsets.all(12),
+                      //textFieldAlignment: MainAxisAlignment.spaceAround,
+        numberOfFields: 4,
+        borderColor: Color(0x0C16325F),
+        //set to true to show as box or false to show as dash
+        showFieldAsBox: true, 
+        fieldWidth: 65,
+        borderRadius: BorderRadius.all(Radius.circular(0)),
+        borderWidth: 1,
+        textStyle: TextStyle(fontSize: 26, color: Color.fromARGB(255, 255, 255, 255)),
+        
+        //runs when a code is typed in
+        // onCodeChanged: (String code) {
+        //     //handle validation or checks here           
+        // },
+        //runs when every textfield is filled
+        onSubmit: (String verificationCode){
+          viewmodel.returnOTP(verificationCode);
+          //Navigator.pop(context, verificationCode);
+            //returnCode(verificationCode);
+        }, // end onSubmit
+    ),
+    Container(
+      child: ResendOTP(),
+      alignment: Alignment.center,
+      //alignment:
+    )
+    
+
+
+                  ],
+                )
+              ));
+        });
+  }
+
+  
+}
+
+// String returnCode(String verificationCode) {
+//   Navigator.pop(context as BuildContext);  
+//    //return verificationCode; 
+// }
