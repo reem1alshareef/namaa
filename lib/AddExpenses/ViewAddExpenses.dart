262973da8275e8713d8f1368880ea67c8b13712b
sadeem74 <sadeem.faisal@outlook.com>
2023-09-30 00:57:48 +0300

    -

--- lib/AddExpenses/ViewAddExpenses.dart
+++ lib/AddExpenses/ViewAddExpenses.dart
@@ -1,4 +1,7 @@
+import 'package:meta/dart2js.dart';
+import 'package:google_fonts/google_fonts.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:namaagp/Components/AccountButton.dart';
 import 'package:namaagp/Components/CostomizedTextButton.dart';
 import 'package:namaagp/Components/TextInputField.dart';
@@ -7,103 +10,197 @@ import 'package:namaagp/AddExpenses/ViewModelAddExpenses.dart';
 import 'package:stacked/stacked.dart';
 import 'package:namaagp/Components/returntopreviouspage.dart';
 
+class ViewAddExpenses extends StatefulWidget {
+  ViewAddExpenses({super.key});
 
-
-
-class ViewAddExpenses extends StatelessWidget {
-  TextEditingController mycontroller = TextEditingController();
-   ViewAddExpenses({super.key});
   @override
+  State<ViewAddExpenses> createState() => _ViewAddExpensesState();
+}
+
+class _ViewAddExpensesState extends State<ViewAddExpenses> {
+  TextEditingController mycontroller = TextEditingController();
+
+  List data = [
+    "شخصي",
+    "بقالة",
+    "عائلة",
+    "فواتير",
+    "ترفيه",
+  ];
+
+  int selectedIndex = -1;
+
   Widget build(BuildContext context) {
+    void _showPopupMenu() async {
+      await showMenu(
+        context: context,
+        position: RelativeRect.fromLTRB(100, 100, 100, 100),
+        items: [
+          PopupMenuItem(
+            child: Text("SR"),
+          ),
+          PopupMenuItem(
+            child: Text("USD"),
+          ),
+          PopupMenuItem(
+            child: Text("AED"),
+          ),
+        ],
+        elevation: 8.0,
+      );
+    }
+
     return ViewModelBuilder<ViewModelAddExpenses>.reactive(
         viewModelBuilder: () => ViewModelAddExpenses(),
         builder: (context, viewmodel, _) {
           return Scaffold(
               backgroundColor: const Color(0x00071121),
               body: Container(
-                padding: const EdgeInsets.only(top: 50.0, left: 10, right: 10),
-                width: 428,
-                height: 926,
-                clipBehavior: Clip.antiAlias,
-                decoration: const BoxDecoration(
-                  //maybe delete const
-                  gradient: LinearGradient(
-                    begin: Alignment(-0.00, -6.00),
-                    end: Alignment(0, 1.5),
-                    colors: [
-                      Color(0xFF342D68),
-                      Color(0xFF352D68),
-                      Color(0x00352D68)
-                    ],
+                  padding:
+                      const EdgeInsets.only(top: 50.0, left: 10, right: 10),
+                  width: 428,
+                  height: 926,
+                  clipBehavior: Clip.antiAlias,
+                  decoration: const BoxDecoration(
+                    //maybe delete const
+                    gradient: LinearGradient(
+                      begin: Alignment(-0.00, -6.00),
+                      end: Alignment(0, 1.5),
+                      colors: [
+                        Color(0xFF342D68),
+                        Color(0xFF352D68),
+                        Color(0x00352D68)
+                      ],
+                    ),
                   ),
-                ),
-                // ignore: prefer_const_constructors
-                child: Column(
-                  children: [
-                    SizedBox(height: 20,),
-                    //ReturnToPreviousPage(title: 'البداية', tWidth: 400),
-                   // introPagesHeader(title: '',subTitle: ''),
-                    mainHeader(title: 'إضافة صرف',),
-                    
-                 Container(
-   width: 80,
-   height: 80,
-  
-  child: Stack(
-    
-children:
-[
-      Positioned(
-        left: 0,
-        top: 0,
-        child: Container(
-          
-          width: 80,
-          height: 80,
-          decoration: ShapeDecoration(
-            color: Color(0xFFB0ACD5),
-            shape: OvalBorder(),
-            shadows: [
-              BoxShadow(
-                color: Color(0xFFB0ACD5),
-                blurRadius: 4,
-                offset: Offset(0, 0),
-                spreadRadius: 0,
-              )
-            ],
-          ),
-        ),
-      ),
-      Positioned(
-        left: 15,
-        top: 15,
-        child: Container(
-          child: Row(mainAxisAlignment: MainAxisAlignment.center,
+                  // ignore: prefer_const_constructors
+                  child: Column(
                     children: [
-                      Flexible(
-                        child: Image.asset(
-                          "assets/Icons/microphone-8-48.png",
-                          height: 30,
-                          width: 30,
-                          fit: BoxFit.contain,
+                      SizedBox(
+                        height: 20,
+                      ),
+                      //ReturnToPreviousPage(title: 'البداية', tWidth: 400),
+                      // introPagesHeader(title: '',subTitle: ''),
+                      mainHeader(
+                        title: 'إضافة صرف',
+                      ),
+
+                      Row(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        // mainAxisAlignment: MainAxisAlignment.center,
+                        children: <Widget>[
+                          TextButton(
+                              onPressed: () async {
+                                _showPopupMenu();
+                              },
+                              child: Image.asset("assets/Icons/down-arrow.png",
+                                  height: 30, width: 30)),
+                          SizedBox(
+                              width: 100,
+                              child: TextField(
+                                  style: TextStyle(
+                                      fontFamily: "Noto Sans Arabic",
+                                      fontSize: 40,
+                                      fontWeight: FontWeight.w500,
+                                      color: Colors.white),
+                                  keyboardType: TextInputType.number,
+                                  inputFormatters: <TextInputFormatter>[
+                                    FilteringTextInputFormatter.digitsOnly
+                                  ])),
+                        ],
+                      ),
+                      ListView.separated(
+                        itemCount: data.length,
+                        scrollDirection: Axis.horizontal,
+                        itemBuilder: (context, index) => FilterChip(
+                          label: Text(data[index]),
+                          selected: index == selectedIndex,
+                          onSelected: (value) {
+                            setState(() {
+                              selectedIndex = index;
+                            });
+                          },
                         ),
-                      ),]),
-          width: 50,
-          height: 50,
-          // decoration: BoxDecoration(
-          //   image: DecorationImage(
-          //     image: AssetImage("assets/Icons/microphone-8-48.png"),
-          //     fit: BoxFit.cover,
-          //   ),
-          // ),
-        ),
-      ),
-                ]),)],
-  ),
-)
-                  ,
-           );
-              
+                        separatorBuilder: (BuildContext context, int index) =>
+                            const SizedBox(
+                          width: 10,
+                        ),
+                      )
+                    ],
+                  )));
         });
   }
 }
+    
+    
+        // child: Text("العملة"))
+
+//                  Container(
+//    width: 80,
+//    height: 80,
+
+//   child: Stack(
+
+// children:
+// [
+//       Positioned(
+//         left: 0,
+//         top: 0,
+//         child: Container(
+
+//           width: 80,
+//           height: 80,
+//           decoration: ShapeDecoration(
+//             color: Color(0xFFB0ACD5),
+//             shape: OvalBorder(),
+//             shadows: [
+//               BoxShadow(
+//                 color: Color(0xFFB0ACD5),
+//                 blurRadius: 4,
+//                 offset: Offset(0, 0),
+//                 spreadRadius: 0,
+//               )
+//             ],
+//           ),
+//         ),
+//       ),
+        // mic button
+        // Positioned(
+        //   left: 15,
+        //   top: 15,
+        //   child: Container(
+        //     child: Row(mainAxisAlignment: MainAxisAlignment.center,
+        //               children: [
+        //                 Flexible(
+        //                   child: Image.asset(
+        //                     "assets/Icons/microphone-8-48.png",
+        //                     height: 30,
+        //                     width: 30,
+        //                     fit: BoxFit.contain,
+
+        // ]
+        // ),
+        //     width: 50,
+        //     height: 50,
+        // decoration: BoxDecoration(
+        //   image: DecorationImage(
+        //     image: AssetImage("assets/Icons/microphone-8-48.png"),
+        //     fit: BoxFit.cover,
+      
+    
+
+//            ),
+//         ),
+//       ),
+//                 ]),)],
+//   ),
+// )
+//                   ,
+//            );
+
+//         });
+//   }
+ 
+ 
+
+  
