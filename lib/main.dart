a62794c4ba8560a1d7a055fb5ae1bf523206b426
sadeem74 <sadeem.faisal@outlook.com>
2023-11-16 06:25:53 +0300

    -

--- lib/main.dart
+++ lib/main.dart
@@ -93,7 +93,7 @@ class _MyHomePageState extends State<MyHomePage> {
           builder: (BuildContext context) {
             submitLogin();
            
-            return ViewAddPlan();
+            return ViewAddExpenses();
             
             //return const ViewSplash();
           },
