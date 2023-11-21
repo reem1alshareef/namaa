2f75f6f4e573ab68fa2ab5242ab13e7ee54d87b0
reem1alshareef <441200087@student.ksu.edu.sa>
2023-11-21 17:50:00 +0300

    ز

--- lib/chat/viewModalChat.dart
+++ lib/chat/viewModalChat.dart
@@ -1,14 +1,38 @@
+// import 'package:namaagp/services/ChatGPTServices.dart';
+// import 'package:stacked/stacked.dart';
+
+// class viewModalChat extends BaseViewModel{
+//   Future<void> getChatGPTAnswer(String question){
+//     Future<void> answer = ChatGPTServices.askQuestion(question);
+//     return  answer;
+//   }
+
+//   Future<void> getDatabaseAnswer(String question){
+//     Future<void> answer = ChatGPTServices.askQuestion(question);
+//     return  answer;
+//   }
+// }
+
 import 'package:namaagp/services/ChatGPTServices.dart';
 import 'package:stacked/stacked.dart';
 
-class viewModalChat extends BaseViewModel{
-  // Future<String?> getChatGPTAnswer(String question){
-  //   Future<String?> answer = ChatGPTServices.askQuestion(question);
-  //   return  answer;
-  // }
+class viewModalChat extends BaseViewModel {
+  Future<String> getChatGPTAnswer(String question) async {
+    // This is where you interact with the ChatGPT API
+    // Replace this with your actual logic to send the question to the API and get the response
+    String answer = await ChatGPTServices.askQuestion(question);
+    return answer;
+  }
 
-  // Future<String?> getDatabaseAnswer(String question){
-  //   Future<String?> answer = ChatGPTServices.askQuestion(question);
-  //   return  answer;
-  // }
+  Future<void> startChat(Function(String) onReceiveResponse) async {
+    // You may need to initialize your ChatGPT connection or setup here
+    // ...
+    // For demonstration purposes, let's simulate a response after initiating the chat
+    onReceiveResponse('ChatGPT response for: 4');
+  }
+
+  void _receiveChatGPTResponse(String answer) {
+    // Notify the UI with the received response
+    notifyListeners();
+  }
 }
--- lib/services/ChatGPTServices.dart
+++ lib/services/ChatGPTServices.dart
@@ -1,24 +1,52 @@
+// import 'package:chat_gpt_sdk/chat_gpt_sdk.dart';
+
+// class ChatGPTServices {
+//   static Future<void> askQuestion(String question) async {
+//   final openAI = OpenAI.instance.build(token: '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',baseOption: HttpSetup(receiveTimeout: const Duration(seconds: 5)),enableLog: true);
+//   final request = CompleteText(prompt:'What is human life expectancy in the United States?',
+//                 model: TextDavinci3Model(), maxTokens: 200);
+
+//  final response = await openAI.onCompletion(request:request);
+//  //final request = ChatCompleteText(messages: [
+//   // final request = ChatCompleteText(messages: [
+//   //   Map.of({'role': "user", 'content': 'Hello!'})
+//   // ], maxToken: 200, model: GptTurboChatModel());
+
+//   // openAI.onChatCompletionSSE(request: request).listen((it) {
+//   //   print(it.choices?.last.message?.content);
+//   // });
+
+//  print('hhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhh');
+//  print(response?.choices.last.text);
+//  print('ddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddd');
+//   }
+
+// }
+
 import 'package:chat_gpt_sdk/chat_gpt_sdk.dart';
 
 class ChatGPTServices {
-  static Future<void> askQuestion() async {
-  final openAI = OpenAI.instance.build(token: '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',baseOption: HttpSetup(receiveTimeout: const Duration(seconds: 5)),enableLog: true);
-  final request = CompleteText(prompt:'What is human life expectancy in the United States?',
-                model: TextDavinci3Model(), maxTokens: 200);
+  static Future<String> askQuestion(String question) async {
+    final openAI = OpenAI.instance.build(
+      token: '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',
+      baseOption: HttpSetup(receiveTimeout: const Duration(seconds: 5)),
+      enableLog: true,
+    );
 
- final response = await openAI.onCompletion(request:request);
- //final request = ChatCompleteText(messages: [
-  // final request = ChatCompleteText(messages: [
-  //   Map.of({'role': "user", 'content': 'Hello!'})
-  // ], maxToken: 200, model: GptTurboChatModel());
+    final request = CompleteText(
+      prompt: question,
+      model: TextDavinci3Model(),
+      maxTokens: 200,
+    );
 
-  // openAI.onChatCompletionSSE(request: request).listen((it) {
-  //   print(it.choices?.last.message?.content);
-  // });
+    final response = await openAI.onCompletion(request: request);
 
- print('hhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhh');
- print(response?.choices.last.text);
- print('ddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddd');
+    if (response != null && response.choices.isNotEmpty) {
+      return response.choices.last.text;
+    } else {
+      // Handle the case when the response is null or empty
+      return 'Failed to get a valid response from the ChatGPT API';
+    }
   }
-
 }
+
